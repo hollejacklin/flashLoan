8cfa54635951ea32b2d7424cc3071137810b6fb5
hollejacklin <91720232+hollejacklin@users.noreply.github.com>
2022-10-18 21:19:58 +0800

    Create balancerFlashLoanDemo.sol

---
+++ balancerFlashLoanDemo.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "./IVault.sol";
+import "./IERC20.sol";
+import "./SafeERC20.sol";
+
+contract myFlashloan {
+    using SafeERC20 for IERC20;
+    
+    function balancerFlashLoan(
+            uint256 loanAmount
+        ) external {
+            
+            address[] memory tokens = new address[](1);
+            uint256[] memory loanAmounts = new uint256[](1);
+            tokens[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+            loanAmounts[0] = loanAmount;
+            IVault (0xBA12222222228d8Ba445958a75a0704d566BF2C8).flashLoan(
+                address(this),
+                tokens,
+                loanAmounts,
+                ""
+            );
+        }
+    function receiveFlashLoan(
+        address[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory data
+    ) external {
+        
+        IERC20(tokens[0]).transfer(msg.sender, amounts[0]+feeAmounts[0]); 
+    }
+}
