a9cadb311c7b1d568700be0a81783625116139de
hollejacklin <91720232+hollejacklin@users.noreply.github.com>
2022-10-18 21:26:28 +0800

    Update balancerFlashLoanDemo.sol

--- balancerFlashLoanDemo.sol
+++ balancerFlashLoanDemo.sol
@@ -10,11 +10,12 @@ contract myFlashloan {
     function balancerFlashLoan(
             uint256 loanAmount
         ) external {
-            
             address[] memory tokens = new address[](1);
             uint256[] memory loanAmounts = new uint256[](1);
-            tokens[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+            //USDC address 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
+            tokens[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; 
             loanAmounts[0] = loanAmount;
+            //Balancer 金庫 0xBA12222222228d8Ba445958a75a0704d566BF2C8
             IVault (0xBA12222222228d8Ba445958a75a0704d566BF2C8).flashLoan(
                 address(this),
                 tokens,
@@ -28,7 +29,7 @@ contract myFlashloan {
         uint256[] memory feeAmounts,
         bytes memory data
     ) external {
-        
+        //借錢後要做的動作。
         IERC20(tokens[0]).transfer(msg.sender, amounts[0]+feeAmounts[0]); 
     }
 }
